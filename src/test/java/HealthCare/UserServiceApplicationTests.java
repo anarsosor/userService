0bcd59374a2f1df5b5318c5c72eee95b64e78a58
anarsosor <anar.sosor@gmail.com>
2023-11-16 23:01:36 -0800

    test

--- src/test/java/HealthCare/UserServiceApplicationTests.java
+++ src/test/java/HealthCare/UserServiceApplicationTests.java
@@ -3,11 +3,15 @@ package HealthCare;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+        assertTrue(true);
 	}
 
 }
